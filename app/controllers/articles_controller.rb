116e7b357f20d529b0d6963eda8d59550e0a5624
meyerhallot <meyerwasup@gmail.com>
2016-10-31 22:30:03 -0400

    Added comments controller and Model. Allows for coments on articles to be made

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,12 +13,12 @@ class ArticlesController < ApplicationController
 
     def create
       @article = Article.new(article_params)
- 
       @article.save
       redirect_to @article
     end
- 
-    private
+    
+
+   
       def article_params
         params.require(:article).permit(:title, :text)
       end
