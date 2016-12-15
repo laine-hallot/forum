8d36026cd899878939dfdb3adc90ecfd5c182869
meyerhallot <meyerwasup@gmail.com>
2016-12-14 19:12:41 -0500

    i don't remember, something about images

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,6 @@ class CommentsController < ApplicationController
  
   private
     def comment_params
-      params.require(:comment).permit(:body)
+      params.require(:comment).permit(:body, :URL)
     end
 end
