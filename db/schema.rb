af86f510ad87dd7d42ba4a05fabc77650534ec05
meyerhallot <meyerwasup@gmail.com>
2016-12-19 02:13:00 -0500

    Local images now display properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211215852) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.text     "URL"
-  end
+ActiveRecord::Schema.define(version: 20161214192739) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
